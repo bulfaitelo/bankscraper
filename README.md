0303e78b01b3b60793b4556de09e971e4ef88a70
Thiago Rodrigues <bulfaitelo@gmail.com>
2018-02-01 16:56:00 -0200

    Login ok

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bankscraper
 Bank Scrapy
+
+Como passar o conta e asenha (senha de acesso web)
+scrapy crawl easynvest -a ac_number=xxxxxx -a password=xxxxxxx
+
--- banckscraper/spiders/easynvest.py
+++ banckscraper/spiders/easynvest.py
@@ -1,11 +1,51 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class EasynvestSpider(scrapy.Spider):
     name = 'easynvest'
-    allowed_domains = ['easynvest.com.br']
-    start_urls = ['http://easynvest.com.br/']
+    allowed_domains = ['www.easynvest.com.br', 'portal.easynvest.com.br']
+    start_urls = [
+        'https://portal.easynvest.com.br/autenticacao/login'
+    ]
+
 
     def parse(self, response):
-        pass
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'Conta': self.ac_number, 'AssinaturaEletronica': self.password},
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        # check login succeed before going on
+        self.log(response.xpath('//title/text()').extract_first())
+
+    # def parse(self, response):     
+        # formdata = {
+        #     'Conta': self.ac_number,
+        #     'AssinaturaEletronica': self.password
+        # }
+        # self.log(formdata)
+        # headers = response.headers
+        
+        # self.log(cookies)
+        # auth_token = ''
+        # for cookie in cookies:
+        #     cookie = cookie.decode('utf-8')
+        #     if 'AuthToken' in cookie:
+        #         auth_token = cookie.split(';')[0].split('=')[1]
+        #         break
+        # self.log(auth_token)       
+        
+        # yield scrapy.FormRequest(
+        #     url='https://portal.easynvest.com.br/autenticacao/login/', 
+        #     method='POST', formdata=formdata, callback=self.parse_login,
+        #     headers=headers
+        # )
+    # def parse_login(self, response):        
+    #     yield scrapy.Request(
+    #         url='https://portal.easynvest.com.br/Financas',
+    #         callback=self.parse_finance
+    #     )
+    # def parse_finance(self, response):
+    #     self.log(response.xpath('//title/text()').extract_first())
